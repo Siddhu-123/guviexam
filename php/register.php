28d931a98523361a3e0c27a0ceef046a94aeb3de
Siddhu-123 <balanagusiddhartha@gmail.com>
2023-12-17 15:43:45 +0530

    unnecessary

--- php/register.php
+++ php/register.php
@@ -1,27 +1,27 @@
 <?php
 
-if (empty($_POST["name"])) {
-    die("Name is required");
-}
+// if (empty($_POST["name"])) {
+//     die("Name is required");
+// }
 
-if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
-    die("Valid email is required");
-}
+// if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+//     die("Valid email is required");
+// }
 
 
-if ( ! preg_match("/[a-z]/i", $_POST["password_confirmation"])) {
-    die("Password must contain at least one letter");
-}
+// if ( ! preg_match("/[a-z]/i", $_POST["password_confirmation"])) {
+//     die("Password must contain at least one letter");
+// }
 
-if ( ! preg_match("/[0-9]/", $_POST["phone"])) {
-    die("Password must contain at least one number");
-}
-if (empty($_POST["dateofbirth"])) {
-    die("date of birth is required");
-}
-if (empty($_POST["address"])) {
-    die("address is required");
-}
+// if ( ! preg_match("/[0-9]/", $_POST["phone"])) {
+//     die("Password must contain at least one number");
+// }
+// if (empty($_POST["dateofbirth"])) {
+//     die("date of birth is required");
+// }
+// if (empty($_POST["address"])) {
+//     die("address is required");
+// }
 
 $password_hash = password_hash($_POST["password_confirmation"], PASSWORD_DEFAULT);
 
